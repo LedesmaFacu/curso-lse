a2de048e3f13b308cf7a62cae52bf03fe261ab72
LedesmaFacu <102493633+LedesmaFacu@users.noreply.github.com>
2025-05-28 13:56:08 -0300

    .
    .

---
+++ workspace_lpc845/01_blinky/main.c
@@ -0,0 +1,22 @@
+#include "board.h"
+
+
+
+int main(void) {
+
+    GPIO_PortInit(GPIO, 1);
+
+    gpio_pin_config_t out_config = { .pinDirection = kGPIO_DigitalOutput, .outputLogic = 1 };
+    GPIO_PinInit(GPIO, 1, 1, &out_config);
+
+    while(1) {
+        
+        GPIO_PinWrite(GPIO, 1, 1, 1);
+        for(uint32_t i=0; i<200000; i++);
+        GPIO_PinWrite(GPIO, 1, 1, 0);
+        for(uint32_t i=0; i<200000; i++);
+
+
+    }
+    return 0;
+}

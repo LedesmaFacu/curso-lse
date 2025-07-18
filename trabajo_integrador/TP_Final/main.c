db7bb7124d069349b509e20a865b2575f680649d
LedesmaFacu <102493633+LedesmaFacu@users.noreply.github.com>
2025-07-18 18:36:37 -0300

    .

--- trabajo_integrador/TP_Final/main.c
+++ trabajo_integrador/TP_Final/main.c
@@ -1,44 +1,106 @@
-
-/*
- * Copyright (c) 2013 - 2015, Freescale Semiconductor, Inc.
- * Copyright 2016-2017 NXP
- * All rights reserved.
- *
- * SPDX-License-Identifier: BSD-3-Clause
- */
-
-#include "fsl_device_registers.h"
-
-#include "pin_mux.h"
-#include "clock_config.h"
-#include "peripherals.h"
-
 #include "board.h"
-#include "app.h"
 
-/*******************************************************************************
- * Definitions
- ******************************************************************************/
+#include "FreeRTOS.h"
+#include "task.h"
 
-/*******************************************************************************
- * Prototypes
- ******************************************************************************/
+#include "app_tasks.h"
 
-/*******************************************************************************
- * Code
- ******************************************************************************/
-/*!
- * @brief Main function
- */
+    // Estructura para crear las tareas
 
-int main(void)
-{
+    // xTaskCreate(TaskCode,     
+    //     "Nombre",
+    //     STACK_SIZE,
+    //     &ucParametertoPass,
+    //     tskIDLE_Priority,
+    //     &xHandle
+    // );
 
-    /* Init board hardware. */
-    BOARD_InitHardware();
+int main(void){
 
-    /* Add user custom codes below */
-    while (1)
-    {
-    }
+        BOARD_BootClockFRO30M();
+        BOARD_InitDebugConsole();
+
+        // 1. Iniciar los periféricos
+        xTaskCreate(task_init,
+            "Iniciar periféricos",
+            tskINIT_STACK, 
+            NULL, 
+            tskINIT_PRIORITY, 
+            NULL);
+
+        // 2. Medicion BH1750
+        xTaskCreate(task_bh1750,
+            "BH1750", 
+            tskBH1750_STACK, 
+            NULL, 
+            tskBH1750_PRIORITY, 
+            NULL);
+
+        // 3. Manejo de setpoint con botones S1 y S2
+        xTaskCreate(task_setpoint,
+             "Setpoint", 
+             tskSETPOINT_STACK, 
+             NULL, 
+             tskSETPOINT_PRIORITY, 
+             NULL);
+
+        // 4. Actualización de display 7 segmentos
+        xTaskCreate(task_display_change, 
+            "Cambiar Display", 
+            tskDISPLAY_CHANGE_STACK,
+             NULL, 
+            tskDISPLAY_CHANGE_PRIORITY,
+             NULL);
+
+        // 5. Control del display 
+        xTaskCreate(task_control, 
+            "Display Control", 
+            tskCONTROL_STACK, 
+            NULL, 
+            tskCONTROL_PRIORITY, 
+            NULL);
+
+        // 6. Escribir en el display
+        xTaskCreate(task_display, 
+             "Escribir Display", 
+             tskDISPLAY_STACK, 
+             NULL, 
+             tskDISPLAY_PRIORITY, 
+             NULL);
+
+        // 7. Control de LED tricolor
+        xTaskCreate(task_tricolor, 
+             "LED Tricolor", 
+             tskTRICOLOR_STACK, 
+             NULL, 
+             tskTRICOLOR_PRIORITY, 
+             NULL);
+
+        // 8. Ajuste de LED azul con RV22
+        xTaskCreate(task_Led_Azul_rv22, 
+             "Led Azul RV22", 
+             tskRV22_STACK, 
+             NULL, 
+             tskRV22_PRIORITY,
+             NULL);
+
+        xTaskCreate(task_cny70, "CNY70_Task", tskBUZZER_STACK, NULL, tskBUZZER_PRIORITY, NULL);
+
+        // 10. Monitoreo
+        xTaskCreate(task_monitoreo, 
+            "Logger", 
+            tskLOGGER_STACK, 
+            NULL, 
+            tskLOGGER_PRIORITY, 
+            NULL);
+
+        // 11. Tarea ADC
+        xTaskCreate(task_adc, 
+            "Tarea ADC", 
+            tskADC_STACK, 
+            NULL, 
+            tskADC_PRIORITY, 
+            NULL);
+
+        vTaskStartScheduler();
 }
